71f6611a1080c25b0fecb616e9903967ea225f8f
justinJSwebdev <trankyhung225@gmail.com>
2024-01-24 18:33:29 +0700

    first

---
+++ LearnNamspace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNameSpace
+{
+    internal class LearnNamspace
+    {
+        public static void XinChao()
+        {
+            Console.WriteLine("Hello Class");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using MyNameSpace;
+namespace Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int x = 1;
+            double y = 1.2;
+            string z = "Hung";
+            char a = '1';
+            bool b = true || false;
+            const char p = '1';
+            int[] n = { 1, 2, 3, 4 };
+            foreach(int temp in n)
+            {
+                Console.WriteLine(temp);
+            }
+            LearnNamspace.XinChao();
+            // Generic
+            // Delegate 
+            //Lambda
+        }
+    }
+}
